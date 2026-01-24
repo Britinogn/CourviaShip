0eac80994b935446543608c41c7633359bbff83a
Tino Nova Tech <124672998+Britinogn@users.noreply.github.com>
2026-01-24 08:20:08 +0100

    CourviaShip app folder structures

---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+
+//const API_BASE_URL = process.env.NUXT_PUBLIC_API_BASE || 'https://rest-api-go-bnnn.onrender.com'
+const API_BASE_URL = process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:5000/api'
+
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  ssr: true,
+  typescript: {
+    typeCheck: true,
+  },
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss(),],
+    server:{
+      watch:{
+        usePolling:true
+      }
+    }
+  },
+
+  modules:['@pinia/nuxt'],
+
+  runtimeConfig:{
+    public:{
+      apiURL: API_BASE_URL
+    }
+  },
+
+  app:{
+    head:{
+
+    }
+  }
+});
